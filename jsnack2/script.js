022f2cf17cc4baae5a482a47029f0e48d682589a
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-11 10:28:13 +0100

    second exercise modified

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,18 +3,12 @@
 let arrayEven = [];
 
 for(i=0; i< 6; i++){
-    let number = parseInt(prompt("inserisci un numero"))
-
-    if(isNaN(number)){
-        alert("inserisci un numero");
+    let number;
+    while(isNaN(number)){
+        number = parseInt(prompt(`${i+1}.inserisci un numero`))
     }
-    if(number % 2 == 0){
-        console.log("hai inserito numero pari");
-    }
-
-    else{
-        console.log(`hai inserito un numero dispari ${number}`)
+    if(number % 2 !== 0){
         arrayEven.push(number);
-        console.log(arrayEven);
     }
 }
+console.log(`i numeri dispari che hai inserito sono: ${arrayEven}`);
